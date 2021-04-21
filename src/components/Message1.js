1eeb96933df1d4eff06ecfa59376d69a2e4d954a
rashfarazzaq123 <rashfarazzaq@gmail.com>
2021-04-21 12:45:55 +0530

    DEV1

--- src/components/Message1.js
+++ src/components/Message1.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import axios from 'axios'
 
-
 axios.defaults.baseURL = 'http://127.0.0.1:5000';
 axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
@@ -42,11 +41,11 @@ class Message1 extends React.Component{
         else{
             recognition.stop()
             recognition.onend=()=>{
-                console.log("stop listening")
+                console.log("Stop listening")
             }
         }
         recognition.onstart=()=>{
-            console.log("Listening")
+            console.log("Listening...")
         }
         
         recognition.onresult=event=>{
@@ -54,24 +53,13 @@ class Message1 extends React.Component{
             for(let i=event.resultIndex; i<event.results.length;i++){
                 const transcript=event.results[i][0].transcript;
                 if(event.results[i].isFinal)this.state.finalTranscript+=transcript+'';
-                else interimTranscript+=transcript;    
+                else interimTranscript+=transcript;   
+               
             }
             
-            const transcriptArr=this.state.finalTranscript.split('')
-            const stopCmd=transcriptArr.slice(-3,-1)
-            console.log('stopCmd')
-
-            if(stopCmd[0]=='stop' && stopCmd[1]=='listening'){
-                recognition.stop()
-                recognition.onend=()=>{
-                    console.log("Stopped per command")
-                    const finalText=transcriptArr.slice(0,-3).join('')
-                    document.getElementById('final').innerHTML=finalText
-                }
-            }
         }
         recognition.onerror=event=>{
-            console.log("Error recognozing:"+event.error)
+            console.log("Error recognizing:"+event.error)
         }
     }
 
@@ -88,6 +76,8 @@ class Message1 extends React.Component{
                 reply.push({from:'you',reply:this.state.finalTranscript});
                 reply.push({from:'convo',reply:res.data});
                 this.setState({chat:reply,message:'',finalTranscript:''});
+                let utterance=new SpeechSynthesisUtterance(res.data);
+                speechSynthesis.speak(utterance);
                 console.log(this.state);
             })
             .catch(err=>{
@@ -101,12 +91,19 @@ class Message1 extends React.Component{
                 reply.push({from:'you',reply:this.state.message});
                 reply.push({from:'convo',reply:res.data});
                 this.setState({chat:reply,message:''});
+                let utterance=new SpeechSynthesisUtterance(res.data);
+                speechSynthesis.speak(utterance);
                 console.log(this.state);
             })
             .catch(err=>{
                 console.log(err)
             })
         }
+    }
+    delete(){
+        let reply=this.state.chat;
+        this.setState({chat:reply,message:''});
+        this.setState({chat:reply,message:'',finalTranscript:''});
     }
     render(){
         return(
@@ -116,26 +113,25 @@ class Message1 extends React.Component{
                     <div>{
                         this.state.chat.map((message)=>{
                             if(message.from=='convo'){
+                                
                                 return <div id="convo_reply" style={{float:'right',marginTop:'4%',marginLeft:'20%',display:'block',fontSize:'14px',backgroundColor:'#79acce',padding:'10px',borderRadius:'10%'}}>{message.reply}<br/></div>
+                                
                             }
                             else{
                                 return <div style={{float:'left',marginBottom:'4%',marginRight:'20%',display:'block',fontSize:'14px',backgroundColor:'#00c0b6',padding:'10px',borderRadius:'10%'}}>{message.reply}<br/></div>
                             }
-                        })   
-                    }
+                        })        
+                        }
                     </div>
                     </div>
                     <div className="textarea">
+                    <button id="micbtn" style={{backgroundImage:'url(/mic.png)',borderRadius:'100%',width:"30px",height:"30px",backgroundSize:'cover',backgroundPosition:'center',margin:'5px'}} onClick={this.toggleListen}/>
                         <input type="text" id="myText" name="message" placeholder="Say something"
                         onChange={(e)=>this.handleInput(e)}
                         className="form-control"
-                        width='500px'
-                        value={this.state.finalTranscript||this.state.message}/>
-                        
-                        <button onClick={()=>this.handleSend()} className="btnsend">
-                            Enter
-                        </button>
-                        <button style={{padding:'2%'}} onClick={this.toggleListen}/>
+                        value={this.state.finalTranscript||this.state.message}/>  
+                        <button style={{backgroundImage:'url(/send.png)',borderRadius:'100%',width:"33px",height:"33px",backgroundSize:'cover',backgroundPosition:'center',margin:'5px'}}  onClick={()=>this.handleSend()} className="btnsend"></button>  
+                        <button id="delbtn"style={{backgroundImage:'url(/delete.png)',width:"33px",height:"33px",backgroundSize:'cover',backgroundPosition:'center',margin:'5px',borderRadius:"16px"}} onClick={()=>this.delete()}></button>
                     </div>
                 </div>
             </div>
--- src/components/Message3.js
+++ src/components/Message3.js
@@ -1,15 +1,66 @@
 import React from 'react'
 import axios from 'axios'
 
-
 axios.defaults.baseURL = 'http://127.0.0.1:5000';
 axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
+const SpeechRecognition =window.webkitSpeechRecognition || window.SpeechRecognition
+const recognition=new SpeechRecognition()
+
+recognition.continous=true
+recognition.interimResults=true
+recognition.lang='en-US'
+
 class Message3 extends React.Component{
-    state={
-        chat:[],
-        message:""
+    constructor(){
+        super()
+        this.state={
+            chat:[],
+            finalTranscript:"",
+            message:"",
+            listening:false
+        }
+        this.toggleListen=this.toggleListen.bind(this)
+        this.handleListen=this.handleListen.bind(this)
+    }
+    toggleListen(){
+        this.setState({
+            listening:!this.state.listening
+        },this.handleListen)
+    }
+    handleListen(){
+        console.log('listening?', this.state.listening)
+        if(this.state.listening){
+            recognition.start()
+            recognition.onend=()=>{
+                console.log("..continue listening..")
+                recognition.start()
+            }
+        }
+        else{
+            recognition.stop()
+            recognition.onend=()=>{
+                console.log("Stop listening")
+            }
+        }
+        recognition.onstart=()=>{
+            console.log("Listening...")
+        }
+        
+        recognition.onresult=event=>{
+            let interimTranscript=''
+            for(let i=event.resultIndex; i<event.results.length;i++){
+                const transcript=event.results[i][0].transcript;
+                if(event.results[i].isFinal)this.state.finalTranscript+=transcript+'';
+                else interimTranscript+=transcript;   
+               
+            }
+            
+        }
+        recognition.onerror=event=>{
+            console.log("Error recognizing:"+event.error)
+        }
     }
 
     handleInput=(e)=>{
@@ -18,13 +69,30 @@ class Message3 extends React.Component{
         
     }
     handleSend=()=>{
-        if(this.state.message!==""){
+        if(this.state.finalTranscript!==""){
+            axios.post('/api/message3',{'message':this.state.finalTranscript})
+            .then(res=>{
+                let reply=this.state.chat;
+                reply.push({from:'you',reply:this.state.finalTranscript});
+                reply.push({from:'convo',reply:res.data});
+                this.setState({chat:reply,message:'',finalTranscript:''});
+                let utterance=new SpeechSynthesisUtterance(res.data);
+                speechSynthesis.speak(utterance);
+                console.log(this.state);
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        }
+        else if(this.state.message!==""){
             axios.post('/api/message3',{'message':this.state.message})
             .then(res=>{
                 let reply=this.state.chat;
                 reply.push({from:'you',reply:this.state.message});
                 reply.push({from:'convo',reply:res.data});
                 this.setState({chat:reply,message:''});
+                let utterance=new SpeechSynthesisUtterance(res.data);
+                speechSynthesis.speak(utterance);
                 console.log(this.state);
             })
             .catch(err=>{
@@ -32,32 +100,38 @@ class Message3 extends React.Component{
             })
         }
     }
+    delete(){
+        let reply=this.state.chat;
+        this.setState({chat:reply,message:''});
+        this.setState({chat:reply,message:'',finalTranscript:''});
+    }
     render(){
         return(
             <div className="Message"> 
                 <div className="wrapper" style={{backgroundImage:'url(/directions.jpg)'}}>
-                    <div className="conversation">
+                <div className="conversation">
                     <div>{
                         this.state.chat.map((message)=>{
                             if(message.from=='convo'){
+                                
                                 return <div id="convo_reply" style={{float:'right',marginTop:'4%',marginLeft:'20%',display:'block',fontSize:'14px',backgroundColor:'#79acce',padding:'10px',borderRadius:'10%'}}>{message.reply}<br/></div>
+                                
                             }
                             else{
                                 return <div style={{float:'left',marginBottom:'4%',marginRight:'20%',display:'block',fontSize:'14px',backgroundColor:'#00c0b6',padding:'10px',borderRadius:'10%'}}>{message.reply}<br/></div>
                             }
-                        })   
-                    }
+                        })        
+                        }
                     </div>
-                    </div>    
+                    </div>
                     <div className="textarea">
+                    <button id="micbtn" style={{backgroundImage:'url(/mic.png)',borderRadius:'100%',width:"30px",height:"30px",backgroundSize:'cover',backgroundPosition:'center',margin:'5px'}} onClick={this.toggleListen}/>
                         <input type="text" id="myText" name="message" placeholder="Say something"
                         onChange={(e)=>this.handleInput(e)}
                         className="form-control"
-                        width='500px'
-                        value={this.state.message}/>
-                        <button onClick={()=>this.handleSend()} className="btnsend">
-                            Enter
-                        </button>
+                        value={this.state.finalTranscript||this.state.message}/>  
+                        <button style={{backgroundImage:'url(/send.png)',borderRadius:'100%',width:"30px",height:"30px",backgroundSize:'cover',backgroundPosition:'center',margin:'5px'}}  onClick={()=>this.handleSend()} className="btnsend"></button>  
+                        <button id="delbtn"style={{backgroundImage:'url(/delete.png)',width:"30px",height:"30px",backgroundSize:'cover',backgroundPosition:'center',margin:'5px'}} onClick={()=>this.delete()}></button>
                     </div>
                 </div>
             </div>
